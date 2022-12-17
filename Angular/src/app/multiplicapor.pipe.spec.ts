96e903168378dbc82a11e2ac8bd079c58f6aed54
conceicao <conceicao-peres@hotmail.com>
2022-12-17 11:07:38 -0300

    finalizando labs: fundamentos angular

--- Angular/src/app/multiplicapor.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MultiplicaporPipe } from './multiplicapor.pipe';
-
-describe('MultiplicaporPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MultiplicaporPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- Angular/src/app/primeiro-componente/primeiro-componente.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "app-primeiro-componente",
-    template: "<img src='./assets/images.png'>",
-    styles: ["img{width: 100%; height: 200px;}"]
-})
-export class PrimeiroComponenteCoponent{}
